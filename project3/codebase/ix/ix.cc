7bbd72a06dff482a4d54682ae390b7f7ed1b7f4e
Keith Heacock <kheacock2@gmail.com>
2019-05-10 14:35:39 -0700

    created a templete for insert and made headers for required helper methods

--- project3/codebase/ix/ix.cc
+++ project3/codebase/ix/ix.cc
@@ -1,11 +1,11 @@
 
 #include "ix.h"
 
-IndexManager* IndexManager::_index_manager = 0;
+IndexManager *IndexManager::_index_manager = 0;
 
-IndexManager* IndexManager::instance()
+IndexManager *IndexManager::instance()
 {
-    if(!_index_manager)
+    if (!_index_manager)
         _index_manager = new IndexManager();
 
     return _index_manager;
@@ -21,6 +21,7 @@ IndexManager::~IndexManager()
 
 RC IndexManager::createFile(const string &fileName)
 {
+    IndexManager::fileName = fileName;
     return -1;
 }
 
@@ -41,27 +42,81 @@ RC IndexManager::closeFile(IXFileHandle &ixfileHandle)
 
 RC IndexManager::insertEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid)
 {
+
     return -1;
 }
 
+//IN PROGRESS
+void IndexManager::insertToTree(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid, int nodePointer, void *newChild)
+{
+    void *pageData = malloc(PAGE_SIZE);
+    ixfileHandle.fh.readPage(nodePointer, pageData);
+    if (!isLeafPage(pageData))
+    {
+        int pagePointer = findTrafficCop(key, attribute, pageData);
+        insertToTree(ixfileHandle, attribute, key, rid, pagePointer, newChild);
+        if (newChild == nullptr)
+        {
+            free(pageData);
+            return;
+        }
+        else
+        {
+            if (willEntryFit(pageData, key, attribute, false))
+            {
+                insertNode(pageData, key, attribute, false);
+                free(pageData);
+                return;
+            }
+            else
+            {
+                newChild = malloc(PAGE_SIZE);
+                splitPage(pageData, newChild);
+                if (isRoot(nodePointer))
+                {
+                    insertNode(pageData, newChild, attribute, false);
+                    //TODO:: UPDATE root pointer global variable
+                }
+                free(pageData);
+                return;
+            }
+        }
+    }
+    else //Is a leaf page
+    {
+        if (willEntryFit(pageData, key, attribute, true))
+        {
+            insertNode(pageData, newChild, attribute, true);
+            free(pageData);
+            return;
+        }
+        else
+        {
+            newChild = malloc(PAGE_SIZE);
+            splitPage(pageData, newChild);
+            free(pageData);
+            return;
+        }
+    }
+}
 RC IndexManager::deleteEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid)
 {
     return -1;
 }
 
-
 RC IndexManager::scan(IXFileHandle &ixfileHandle,
-        const Attribute &attribute,
-        const void      *lowKey,
-        const void      *highKey,
-        bool			lowKeyInclusive,
-        bool        	highKeyInclusive,
-        IX_ScanIterator &ix_ScanIterator)
+                      const Attribute &attribute,
+                      const void *lowKey,
+                      const void *highKey,
+                      bool lowKeyInclusive,
+                      bool highKeyInclusive,
+                      IX_ScanIterator &ix_ScanIterator)
 {
     return -1;
 }
 
-void IndexManager::printBtree(IXFileHandle &ixfileHandle, const Attribute &attribute) const {
+void IndexManager::printBtree(IXFileHandle &ixfileHandle, const Attribute &attribute) const
+{
 }
 
 IX_ScanIterator::IX_ScanIterator()
@@ -82,7 +137,6 @@ RC IX_ScanIterator::close()
     return -1;
 }
 
-
 IXFileHandle::IXFileHandle()
 {
     ixReadPageCounter = 0;
@@ -98,4 +152,3 @@ RC IXFileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePa
 {
     return -1;
 }
-
--- project3/codebase/ix/ix.h
+++ project3/codebase/ix/ix.h
@@ -6,80 +6,113 @@
 
 #include "../rbf/rbfm.h"
 
-# define IX_EOF (-1)  // end of the index scan
+#define IX_EOF (-1) // end of the index scan
 
 class IX_ScanIterator;
 class IXFileHandle;
 
-class IndexManager {
+class IndexManager
+{
 
-    public:
-        static IndexManager* instance();
+public:
+    static IndexManager *instance();
 
-        // Create an index file.
-        RC createFile(const string &fileName);
+    // Create an index file.
+    RC createFile(const string &fileName);
 
-        // Delete an index file.
-        RC destroyFile(const string &fileName);
+    // Delete an index file.
+    RC destroyFile(const string &fileName);
 
-        // Open an index and return an ixfileHandle.
-        RC openFile(const string &fileName, IXFileHandle &ixfileHandle);
+    // Open an index and return an ixfileHandle.
+    RC openFile(const string &fileName, IXFileHandle &ixfileHandle);
 
-        // Close an ixfileHandle for an index.
-        RC closeFile(IXFileHandle &ixfileHandle);
+    // Close an ixfileHandle for an index.
+    RC closeFile(IXFileHandle &ixfileHandle);
 
-        // Insert an entry into the given index that is indicated by the given ixfileHandle.
-        RC insertEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid);
+    // Insert an entry into the given index that is indicated by the given ixfileHandle.
+    RC insertEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid);
 
-        // Delete an entry from the given index that is indicated by the given ixfileHandle.
-        RC deleteEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid);
+    // Delete an entry from the given index that is indicated by the given ixfileHandle.
+    RC deleteEntry(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid);
 
-        // Initialize and IX_ScanIterator to support a range search
-        RC scan(IXFileHandle &ixfileHandle,
-                const Attribute &attribute,
-                const void *lowKey,
-                const void *highKey,
-                bool lowKeyInclusive,
-                bool highKeyInclusive,
-                IX_ScanIterator &ix_ScanIterator);
+    // Initialize and IX_ScanIterator to support a range search
+    RC scan(IXFileHandle &ixfileHandle,
+            const Attribute &attribute,
+            const void *lowKey,
+            const void *highKey,
+            bool lowKeyInclusive,
+            bool highKeyInclusive,
+            IX_ScanIterator &ix_ScanIterator);
 
-        // Print the B+ tree in pre-order (in a JSON record format)
-        void printBtree(IXFileHandle &ixfileHandle, const Attribute &attribute) const;
+    // Print the B+ tree in pre-order (in a JSON record format)
+    void printBtree(IXFileHandle &ixfileHandle, const Attribute &attribute) const;
 
-    protected:
-        IndexManager();
-        ~IndexManager();
+protected:
+    IndexManager();
+    ~IndexManager();
 
-    private:
-        static IndexManager *_index_manager;
+private:
+    static IndexManager *_index_manager;
+    string fileName;
+    //Pre: page is a pointer to page data of any type
+    //Post: the truth value of whether the page parameter is a leaf
+    bool isLeafPage(const void *page);
+
+    //Pre: val contains a valid leaf entry to be inserted and attr corresponding to that entry
+    //Post: return the total size of the entry including size of RID
+    int findLeafEntrySize(const void *val, const Attribute attr);
+
+    //Pre: page is a pointer to a page data of any type
+    //Post: the offset of the page’s free space is returned
+    int freeSpaceOffset(void *page);
+
+    //Pre: val contains a valid entry that will be inserted to a non-leaf page and attr corresponding to that entry
+    //Post: return the total size of the entry which will be equal to the key size
+    int findTrafficCopSize(const void *val, const Attribute attr);
+
+    //Pre: val contains a valid entry to be inserted either to a Leaf or Non-Leaf page and attr corresponding to that entry.
+    //     &Page is a pointer to a FileHandle of a page
+    //Post: returns whether or not the given val will fit on the given page.
+    bool willEntryFit(const void *pageData, const void *val, const Attribute attr, bool isLeafValue);
+
+    //Pre: val contains a valid entry to be inserted and attr corresponding to that entry. &page is a pointer to a FileHandle of a
+    //     Non-Leaf page
+    //Post: returns the page number corresponding to either the next level down
+    int findTrafficCop(const void *val, const Attribute attr, const void *pageData);
+
+    void insertNode(void *page, const void *key, const Attribute &attr, bool isLeafNode);
+
+    void splitPage(void *inPage, void *newChildEntry);
+
+    void insertToTree(IXFileHandle &ixfileHandle, const Attribute &attribute, const void *key, const RID &rid, int nodePointer, void *newChild);
+
+    bool isRoot(int pageNumber);
 };
 
+class IX_ScanIterator
+{
+public:
+    // Constructor
+    IX_ScanIterator();
 
-class IX_ScanIterator {
-    public:
+    // Destructor
+    ~IX_ScanIterator();
 
-		// Constructor
-        IX_ScanIterator();
+    // Get next matching entry
+    RC getNextEntry(RID &rid, void *key);
 
-        // Destructor
-        ~IX_ScanIterator();
-
-        // Get next matching entry
-        RC getNextEntry(RID &rid, void *key);
-
-        // Terminate index scan
-        RC close();
+    // Terminate index scan
+    RC close();
 };
 
-
-
-class IXFileHandle {
-    public:
-
+class IXFileHandle
+{
+public:
     // variables to keep counter for each operation
     unsigned ixReadPageCounter;
     unsigned ixWritePageCounter;
     unsigned ixAppendPageCounter;
+    FileHandle fh;
 
     // Constructor
     IXFileHandle();
@@ -87,9 +120,8 @@ class IXFileHandle {
     // Destructor
     ~IXFileHandle();
 
-	// Put the current counter values of associated PF FileHandles into variables
-	RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount);
-
+    // Put the current counter values of associated PF FileHandles into variables
+    RC collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount);
 };
 
 #endif
