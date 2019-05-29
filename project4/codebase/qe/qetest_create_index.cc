94b7829b0f227ed2888ee68d31486b1b4b20f5a3
Allston Mickey <allston.mickey@gmail.com>
2019-05-28 23:14:42 -0400

    More formatting

--- project4/codebase/qe/qetest_create_index.cc
+++ project4/codebase/qe/qetest_create_index.cc
@@ -8,20 +8,20 @@
 #include "qe_test_util.h"
 
 int TestCreateIndex() {
-	// Mandatory for all
-	// Create an Index
-	// Load Data
-	// Create an Index
+    // Mandatory for all
+    // Create an Index
+    // Load Data
+    // Create an Index
 
-	RC rc = success;
-	cerr << endl << "***** In QE Test Create Index *****" << endl;
+    RC rc = success;
+    cerr << endl << "***** In QE Test Create Index *****" << endl;
 
-	// Create an index before inserting tuples.
-	rc = createIndexforLeftB();
-	if (rc != success) {
-		cerr << "***** createIndexforLeftB() failed.  *****" << endl;
-		return rc;
-	}
+    // Create an index before inserting tuples.
+    rc = createIndexforLeftB();
+    if (rc != success) {
+        cerr << "***** createIndexforLeftB() failed.  *****" << endl;
+        return rc;
+    }
 
     // Scan the index catalog for entries on table "left", get the attribute name.
     RM_ScanIterator rmsi;
@@ -35,8 +35,8 @@ int TestCreateIndex() {
     vector<string> projectedAttributes { INDEXES_COL_COLUMN_NAME };
     rc = rm->scan(INDEXES_TABLE_NAME, INDEXES_COL_TABLE_NAME, EQ_OP, compValue, projectedAttributes, rmsi);
     if (rc != SUCCESS) {
-		cerr << "***** scan() failed.  *****" << endl;
-		return rc;
+        cerr << "***** scan() failed.  *****" << endl;
+        return rc;
     }
 
     // For all indexes on table "left", try to find index on attribute "B".
@@ -63,29 +63,29 @@ int TestCreateIndex() {
     free(data);
     if (!attributeFound)
     {
-		cerr << "***** Finding table & attribute in index catalog failed.  *****" << endl;
-		return -1;
+        cerr << "***** Finding table & attribute in index catalog failed.  *****" << endl;
+        return -1;
     }
 
     // Try to create index again.  Should fail.
-	rc = createIndexforLeftB();
-	if (rc == success) {
-		cerr << "***** Duplicate createIndexforLeftB() should fail.  *****" << endl;
-		return rc;
-	}
+    rc = createIndexforLeftB();
+    if (rc == success) {
+        cerr << "***** Duplicate createIndexforLeftB() should fail.  *****" << endl;
+        return rc;
+    }
 
     // Try to create index on nonexistent table.  Should fail.
-	rc = createIndexforNonexistentTable();
-	if (rc == success) {
-		cerr << "***** createIndexforNonexistentTable() should fail.  *****" << endl;
-		return rc;
-	}
+    rc = createIndexforNonexistentTable();
+    if (rc == success) {
+        cerr << "***** createIndexforNonexistentTable() should fail.  *****" << endl;
+        return rc;
+    }
 
     // Try to create index on existent table but nonexistent attribute.
     rc = createIndexforLeftNonexistentAttribute();
     if (rc == success) {
-		cerr << "***** createIndexforLeftNonexistentAttribute() should fail.  *****" << endl;
-		return rc;
+        cerr << "***** createIndexforLeftNonexistentAttribute() should fail.  *****" << endl;
+        return rc;
     }
 
     return SUCCESS;
@@ -93,29 +93,29 @@ int TestCreateIndex() {
 
 
 int main() {
-	// Tables created: left
-	// Indexes created: left.B, left.C
+    // Tables created: left
+    // Indexes created: left.B, left.C
 
-	// Initialize the system catalog
-	if (deleteAndCreateCatalog() != success) {
-		cerr << "***** deleteAndCreateCatalog() failed." << endl;
-		cerr << "***** [FAIL] QE Test Create Index failed. *****" << endl;
-		return fail;
-	}
+    // Initialize the system catalog
+    if (deleteAndCreateCatalog() != success) {
+        cerr << "***** deleteAndCreateCatalog() failed." << endl;
+        cerr << "***** [FAIL] QE Test Create Index failed. *****" << endl;
+        return fail;
+    }
 
-	
-	// Create the left table
-	if (createLeftTable() != success) {
-		cerr << "***** createLeftTable() failed." << endl;
-		cerr << "***** [FAIL] QE Test Create Index failed. *****" << endl;
-		return fail;
-	}
+    
+    // Create the left table
+    if (createLeftTable() != success) {
+        cerr << "***** createLeftTable() failed." << endl;
+        cerr << "***** [FAIL] QE Test Create Index failed. *****" << endl;
+        return fail;
+    }
 
-	if (TestCreateIndex() != success) {
-		cerr << "***** [FAIL] QE Test Create Index failed. *****" << endl;
-		return fail;
-	} else {
-		cerr << "***** QE Test Create Index finished. The result will be examined. *****" << endl;
-		return success;
-	}
+    if (TestCreateIndex() != success) {
+        cerr << "***** [FAIL] QE Test Create Index failed. *****" << endl;
+        return fail;
+    } else {
+        cerr << "***** QE Test Create Index finished. The result will be examined. *****" << endl;
+        return success;
+    }
 }
