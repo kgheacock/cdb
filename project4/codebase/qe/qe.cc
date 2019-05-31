eb296e335842c5f4693a9d5bf5ae6c48191e6eeb
Keith Heacock <kheacock2@gmail.com>
2019-05-31 13:18:20 -0700

    added compare functionallity to Value struct

--- project4/codebase/qe/qe.cc
+++ project4/codebase/qe/qe.cc
@@ -1,7 +1,98 @@
 
 #include "qe.h"
-
-Filter::Filter(Iterator* input, const Condition &condition) {
+#include <string.h>
+Filter::Filter(Iterator *input, const Condition &condition)
+{
+}
+int Value::compare(const int key, const int value)
+{
+    if (key == value)
+        return 0;
+    if (key > value)
+        return 1;
+    if (key < value)
+        return -1;
+    return 0; // suppress warnings
 }
 
+int Value::compare(const float key, const float value)
+{
+    if (key == value)
+        return 0;
+    if (key > value)
+        return 1;
+    if (key < value)
+        return -1;
+    return 0;
+}
+int Value::compare(const char *key, const char *value)
+{
+    return strcmp(key, value);
+}
+int Value::compare(const void *key, const void *value, const AttrType attrType)
+{
+    switch (attrType)
+    {
+    case TypeInt:
+    {
+        int key_int = *(int *)key;
+        int value_int = *(int *)value;
+        return compare(key_int, value_int);
+    }
+    case TypeReal:
+    {
+        float key_float = *(float *)key;
+        float value_float = *(float *)value;
+        return compare(key_float, value_float);
+    }
+    case TypeVarChar:
+    {
+        size_t key_size;
+        size_t value_size;
+        memcpy(&key_size, key, sizeof(uint32_t));
+        memcpy(&value_size, value, sizeof(uint32_t));
+        char key_array[key_size + 1];
+        char value_array[value_size + 1];
+        memcpy(key_array, (char *)key + sizeof(uint32_t), key_size);
+        memcpy(value_array, (char *)value + sizeof(uint32_t), value_size);
+        key_array[key_size] = '\0';
+        value_array[value_size] = '\0';
+        return compare(key_array, value_array);
+    }
+    }
+    throw "Attribute is malformed";
+    return -2;
+}
+int Value::compare(const Value *rhs)
+{
+    if (rhs->type != type)
+    {
+        throw "Mismatched attributes";
+    }
+    return compare(data, rhs->data, type);
+}
+bool Value::compare(const Value *rhs, const CompOp op)
+{
+    int result = compare(rhs);
+    switch (op)
+    {
+    case LT_OP:
+        return result == -1;
+        break;
+    case GT_OP:
+        return result == 1;
+        break;
+    case LE_OP:
+        return result <= 0;
+        break;
+    case GE_OP:
+        return result >= 0;
+        break;
+    case EQ_OP:
+        return result == 0;
+        break;
+    }
+    throw "Comparison Operator is invalid";
+    return false;
+}
 // ... the rest of your implementations go here
--- project4/codebase/qe/qe.h
+++ project4/codebase/qe/qe.h
@@ -7,229 +7,241 @@
 #include "../rm/rm.h"
 #include "../ix/ix.h"
 
-#define QE_EOF (-1)  // end of the index scan
+#define QE_EOF (-1) // end of the index scan
 
 using namespace std;
 
-typedef enum{ MIN=0, MAX, COUNT, SUM, AVG } AggregateOp;
+typedef enum
+{
+    MIN = 0,
+    MAX,
+    COUNT,
+    SUM,
+    AVG
+} AggregateOp;
 
 // The following functions use the following
 // format for the passed data.
 //    For INT and REAL: use 4 bytes
 //    For VARCHAR: use 4 bytes for the length followed by the characters
 
-struct Value {
-    AttrType type;          // type of value
-    void     *data;         // value
+struct Value
+{
+    AttrType type; // type of value
+    void *data;    // value
+    int compare(const Value *rhs);
+    bool compare(const Value *rhs, const CompOp op);
+    int compare(const void *key, const void *value, const AttrType attrType);
+    int compare(const int key, const int value);
+    int compare(const float key, const float value);
+    int compare(const char *key, const char *value);
 };
 
-
-struct Condition {
-    string  lhsAttr;        // left-hand side attribute
-    CompOp  op;             // comparison operator
-    bool    bRhsIsAttr;     // TRUE if right-hand side is an attribute and not a value; FALSE, otherwise.
-    string  rhsAttr;        // right-hand side attribute if bRhsIsAttr = TRUE
-    Value   rhsValue;       // right-hand side value if bRhsIsAttr = FALSE
+struct Condition
+{
+    string lhsAttr;  // left-hand side attribute
+    CompOp op;       // comparison operator
+    bool bRhsIsAttr; // TRUE if right-hand side is an attribute and not a value; FALSE, otherwise.
+    string rhsAttr;  // right-hand side attribute if bRhsIsAttr = TRUE
+    Value rhsValue;  // right-hand side value if bRhsIsAttr = FALSE
 };
 
-
-class Iterator {
+class Iterator
+{
     // All the relational operators and access methods are iterators.
-    public:
-        virtual RC getNextTuple(void *data) = 0;
-        virtual void getAttributes(vector<Attribute> &attrs) const = 0;
-        virtual ~Iterator() {};
+public:
+    virtual RC getNextTuple(void *data) = 0;
+    virtual void getAttributes(vector<Attribute> &attrs) const = 0;
+    virtual ~Iterator(){};
 };
-
 
 class TableScan : public Iterator
 {
     // A wrapper inheriting Iterator over RM_ScanIterator
-    public:
-        RelationManager &rm;
-        RM_ScanIterator *iter;
-        string tableName;
-        vector<Attribute> attrs;
-        vector<string> attrNames;
-        RID rid;
+public:
+    RelationManager &rm;
+    RM_ScanIterator *iter;
+    string tableName;
+    vector<Attribute> attrs;
+    vector<string> attrNames;
+    RID rid;
 
-        TableScan(RelationManager &rm, const string &tableName, const char *alias = NULL):rm(rm)
+    TableScan(RelationManager &rm, const string &tableName, const char *alias = NULL) : rm(rm)
+    {
+        //Set members
+        this->tableName = tableName;
+
+        // Get Attributes from RM
+        rm.getAttributes(tableName, attrs);
+
+        // Get Attribute Names from RM
+        unsigned i;
+        for (i = 0; i < attrs.size(); ++i)
         {
-        	//Set members
-        	this->tableName = tableName;
+            // convert to char *
+            attrNames.push_back(attrs.at(i).name);
+        }
 
-            // Get Attributes from RM
-            rm.getAttributes(tableName, attrs);
+        // Call RM scan to get an iterator
+        iter = new RM_ScanIterator();
+        rm.scan(tableName, "", NO_OP, NULL, attrNames, *iter);
 
-            // Get Attribute Names from RM
-            unsigned i;
-            for(i = 0; i < attrs.size(); ++i)
-            {
-                // convert to char *
-                attrNames.push_back(attrs.at(i).name);
-            }
+        // Set alias
+        if (alias)
+            this->tableName = alias;
+    };
 
-            // Call RM scan to get an iterator
-            iter = new RM_ScanIterator();
-            rm.scan(tableName, "", NO_OP, NULL, attrNames, *iter);
+    // Start a new iterator given the new compOp and value
+    void setIterator()
+    {
+        iter->close();
+        delete iter;
+        iter = new RM_ScanIterator();
+        rm.scan(tableName, "", NO_OP, NULL, attrNames, *iter);
+    };
 
-            // Set alias
-            if(alias) this->tableName = alias;
-        };
+    RC getNextTuple(void *data)
+    {
+        return iter->getNextTuple(rid, data);
+    };
 
-        // Start a new iterator given the new compOp and value
-        void setIterator()
+    void getAttributes(vector<Attribute> &attrs) const
+    {
+        attrs.clear();
+        attrs = this->attrs;
+        unsigned i;
+
+        // For attribute in vector<Attribute>, name it as rel.attr
+        for (i = 0; i < attrs.size(); ++i)
         {
-            iter->close();
-            delete iter;
-            iter = new RM_ScanIterator();
-            rm.scan(tableName, "", NO_OP, NULL, attrNames, *iter);
-        };
+            string tmp = tableName;
+            tmp += ".";
+            tmp += attrs.at(i).name;
+            attrs.at(i).name = tmp;
+        }
+    };
 
-        RC getNextTuple(void *data)
-        {
-            return iter->getNextTuple(rid, data);
-        };
-
-        void getAttributes(vector<Attribute> &attrs) const
-        {
-            attrs.clear();
-            attrs = this->attrs;
-            unsigned i;
-
-            // For attribute in vector<Attribute>, name it as rel.attr
-            for(i = 0; i < attrs.size(); ++i)
-            {
-                string tmp = tableName;
-                tmp += ".";
-                tmp += attrs.at(i).name;
-                attrs.at(i).name = tmp;
-            }
-        };
-
-        ~TableScan()
-        {
-        	iter->close();
-        };
+    ~TableScan()
+    {
+        iter->close();
+    };
 };
-
 
 class IndexScan : public Iterator
 {
     // A wrapper inheriting Iterator over IX_IndexScan
-    public:
-        RelationManager &rm;
-        RM_IndexScanIterator *iter;
-        string tableName;
-        string attrName;
-        vector<Attribute> attrs;
-        char key[PAGE_SIZE];
-        RID rid;
+public:
+    RelationManager &rm;
+    RM_IndexScanIterator *iter;
+    string tableName;
+    string attrName;
+    vector<Attribute> attrs;
+    char key[PAGE_SIZE];
+    RID rid;
 
-        IndexScan(RelationManager &rm, const string &tableName, const string &attrName, const char *alias = NULL):rm(rm)
+    IndexScan(RelationManager &rm, const string &tableName, const string &attrName, const char *alias = NULL) : rm(rm)
+    {
+        // Set members
+        this->tableName = tableName;
+        this->attrName = attrName;
+
+        // Get Attributes from RM
+        rm.getAttributes(tableName, attrs);
+
+        // Call rm indexScan to get iterator
+        iter = new RM_IndexScanIterator();
+        rm.indexScan(tableName, attrName, NULL, NULL, true, true, *iter);
+
+        // Set alias
+        if (alias)
+            this->tableName = alias;
+    };
+
+    // Start a new iterator given the new key range
+    void setIterator(void *lowKey,
+                     void *highKey,
+                     bool lowKeyInclusive,
+                     bool highKeyInclusive)
+    {
+        iter->close();
+        delete iter;
+        iter = new RM_IndexScanIterator();
+        rm.indexScan(tableName, attrName, lowKey, highKey, lowKeyInclusive,
+                     highKeyInclusive, *iter);
+    };
+
+    RC getNextTuple(void *data)
+    {
+        int rc = iter->getNextEntry(rid, key);
+        if (rc == 0)
         {
-        	// Set members
-        	this->tableName = tableName;
-        	this->attrName = attrName;
+            rc = rm.readTuple(tableName.c_str(), rid, data);
+        }
+        return rc;
+    };
 
+    void getAttributes(vector<Attribute> &attrs) const
+    {
+        attrs.clear();
+        attrs = this->attrs;
+        unsigned i;
 
-            // Get Attributes from RM
-            rm.getAttributes(tableName, attrs);
-
-            // Call rm indexScan to get iterator
-            iter = new RM_IndexScanIterator();
-            rm.indexScan(tableName, attrName, NULL, NULL, true, true, *iter);
-
-            // Set alias
-            if(alias) this->tableName = alias;
-        };
-
-        // Start a new iterator given the new key range
-        void setIterator(void* lowKey,
-                         void* highKey,
-                         bool lowKeyInclusive,
-                         bool highKeyInclusive)
+        // For attribute in vector<Attribute>, name it as rel.attr
+        for (i = 0; i < attrs.size(); ++i)
         {
-            iter->close();
-            delete iter;
-            iter = new RM_IndexScanIterator();
-            rm.indexScan(tableName, attrName, lowKey, highKey, lowKeyInclusive,
-                           highKeyInclusive, *iter);
-        };
+            string tmp = tableName;
+            tmp += ".";
+            tmp += attrs.at(i).name;
+            attrs.at(i).name = tmp;
+        }
+    };
 
-        RC getNextTuple(void *data)
-        {
-            int rc = iter->getNextEntry(rid, key);
-            if(rc == 0)
-            {
-                rc = rm.readTuple(tableName.c_str(), rid, data);
-            }
-            return rc;
-        };
-
-        void getAttributes(vector<Attribute> &attrs) const
-        {
-            attrs.clear();
-            attrs = this->attrs;
-            unsigned i;
-
-            // For attribute in vector<Attribute>, name it as rel.attr
-            for(i = 0; i < attrs.size(); ++i)
-            {
-                string tmp = tableName;
-                tmp += ".";
-                tmp += attrs.at(i).name;
-                attrs.at(i).name = tmp;
-            }
-        };
-
-        ~IndexScan()
-        {
-            iter->close();
-        };
+    ~IndexScan()
+    {
+        iter->close();
+    };
 };
 
-
-class Filter : public Iterator {
+class Filter : public Iterator
+{
     // Filter operator
-    public:
-        Filter(Iterator *input,               // Iterator of input R
-               const Condition &condition     // Selection condition
-        );
-        ~Filter(){};
+public:
+    Filter(Iterator *input,           // Iterator of input R
+           const Condition &condition // Selection condition
+    );
+    ~Filter(){};
 
-        RC getNextTuple(void *data) {return QE_EOF;};
-        // For attribute in vector<Attribute>, name it as rel.attr
-        void getAttributes(vector<Attribute> &attrs) const{};
+    RC getNextTuple(void *data) { return QE_EOF; };
+    // For attribute in vector<Attribute>, name it as rel.attr
+    void getAttributes(vector<Attribute> &attrs) const {};
 };
 
-
-class Project : public Iterator {
+class Project : public Iterator
+{
     // Projection operator
-    public:
-        Project(Iterator *input,                    // Iterator of input R
-              const vector<string> &attrNames){};   // vector containing attribute names
-        ~Project(){};
+public:
+    Project(Iterator *input,                    // Iterator of input R
+            const vector<string> &attrNames){}; // vector containing attribute names
+    ~Project(){};
 
-        RC getNextTuple(void *data) {return QE_EOF;};
-        // For attribute in vector<Attribute>, name it as rel.attr
-        void getAttributes(vector<Attribute> &attrs) const{};
+    RC getNextTuple(void *data) { return QE_EOF; };
+    // For attribute in vector<Attribute>, name it as rel.attr
+    void getAttributes(vector<Attribute> &attrs) const {};
 };
 
-
-class INLJoin : public Iterator {
+class INLJoin : public Iterator
+{
     // Index nested-loop join operator
-    public:
-        INLJoin(Iterator *leftIn,           // Iterator of input R
-               IndexScan *rightIn,          // IndexScan Iterator of input S
-               const Condition &condition   // Join condition
-        ){};
-        ~INLJoin(){};
+public:
+    INLJoin(Iterator *leftIn,          // Iterator of input R
+            IndexScan *rightIn,        // IndexScan Iterator of input S
+            const Condition &condition // Join condition
+    ){};
+    ~INLJoin(){};
 
-        RC getNextTuple(void *data){return QE_EOF;};
-        // For attribute in vector<Attribute>, name it as rel.attr
-        void getAttributes(vector<Attribute> &attrs) const{};
+    RC getNextTuple(void *data) { return QE_EOF; };
+    // For attribute in vector<Attribute>, name it as rel.attr
+    void getAttributes(vector<Attribute> &attrs) const {};
 };
-
 
 #endif
